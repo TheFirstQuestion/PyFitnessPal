a8352913c734c9b0fd0bde6eb8c12b098131adf2
Sophie Opferman <sophieopferman6@gmail.com>
2017-08-16 14:35:49 -0400

    improve user creation

--- database.py
+++ database.py
@@ -4,163 +4,173 @@ class Database:
     def __init__(self):
         self.conn = sqlite3.connect('pyfitnesspal.db')
 
-    def basicStop(self):
+    def commit(self):
+        self.conn.commit()
+
+    def close(self):
         self.conn.commit()
         self.conn.close()
 
     def basicInsert(self,text):
         print(text)
 
-    def main(self):
+    def makeNew(self):
         # Create USERS table
-        conn.execute('''CREATE TABLE USERS
-                (ID             INT        PRIMARY KEY      NOT NULL,
-                EMAIL           VARCHAR                     NOT NULL,
-                PASSWORD        TEXT                        NOT NULL,
-                AGE             INT                         NOT NULL,
-                SEX             CHAR(1)                     NOT NULL,
-                HEIGHT          INT                         NOT NULL,
-                WEIGHT          INT                         NOT NULL,
-                ACTIVITY        INT                         NOT NULL,
-                NAME            VARCHAR                     NOT NULL
+        self.conn.execute('''CREATE TABLE USERS
+                (ID             INTEGER        PRIMARY KEY      NOT NULL,
+                EMAIL           VARCHAR                     ,
+                PASSWORD        TEXT                        ,
+                AGE             INT                         ,
+                SEX             CHAR(1)                     ,
+                HEIGHT          INT                         ,
+                WEIGHT          INT                         ,
+                ACTIVITY        INT                         ,
+                NAME            VARCHAR
                  );''')
 
         # Create EATING table
-        conn.execute('''CREATE TABLE EATING
-               (ID             INT         PRIMARY KEY      NOT NULL,
-                USER           INT                          NOT NULL,
-                FOOD           INT                          NOT NULL,
-                MEASURE        INT                          NOT NULL,
-                SERVINGS       INT                          NOT NULL,
-                DAY            TEXT                         NOT NULL
+        self.conn.execute('''CREATE TABLE EATING
+               (ID             INTEGER         PRIMARY KEY      NOT NULL,
+                USER           INT                          ,
+                FOOD           INT                          ,
+                MEASURE        INT                          ,
+                SERVINGS       INT                          ,
+                DAY            TEXT
                  );''')
 
         # Create NUTRIENTS table
-        conn.execute('''CREATE TABLE NUTRIENTS
-               (ID             INT         PRIMARY KEY      NOT NULL,
-                NAME           VARCHAR                      NOT NULL,
-                UNIT           VARCHAR                      NOT NULL,
-                CAT          VARCHAR                      NOT NULL
+        self.conn.execute('''CREATE TABLE NUTRIENTS
+               (ID             INTEGER         PRIMARY KEY      NOT NULL,
+                NAME           VARCHAR                      ,
+                UNIT           VARCHAR                      ,
+                CAT            VARCHAR
                  );''')
 
         # Add nutrient information
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (0, 'Water', 'g', 'Proximates')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (1, 'Energy', 'calories', 'Proximates')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (2, 'Protein', 'g', 'Proximates')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (3, 'Total Lipid (fat)', 'g', 'Proximates')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (4, 'Carbohydrate', 'g', 'Proximates')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (5, 'Fiber', 'g', 'Proximates')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (6, 'Sugars', 'g', 'Proximates')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (7, 'Calcium', 'mg', 'Minerals')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (8, 'Iron', 'mg', 'Minerals')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (9, 'Magnesium', 'mg', 'Minerals')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (10, 'Phosphorus', 'mg', 'Minerals')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (11, 'Potassium', 'mg', 'Minerals')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (12, 'Sodium', 'mg', 'Minerals')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (13, 'Zinc', 'mg', 'Minerals')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (14, 'Vitamin C', 'mg', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (15, 'Thiamin', 'mg', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (16, 'Riboflavin', 'mg', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (17, 'Niacin', 'mg', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (18, 'Vitamin B-6', 'mg', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (19, 'Folate', '&#181;g', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (20, 'Vitamin B-12', '&#181;g', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (21, 'Vitamin A, RAE', '&#181;g', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (22, 'Vitamin A, IU', 'IU', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (23, 'Vitamin E', 'mg', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (24, 'Vitamin D (D2 + D3)', '&#181;g', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (25, 'Vitamin D', 'IU', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (26, 'Vitamin K', '&#181;g', 'Vitamins')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (27, 'Saturated Fat', 'g', 'Lipids')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (28, 'Monounsaturated Fat', 'g', 'Lipids')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (29, 'Polyunsaturated Fat', 'g', 'Lipids')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (30, 'Trans Fat', 'g', 'Lipids')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (31, 'Cholesterol', 'mg', 'Lipids')");
 
-        conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
+        self.conn.execute("INSERT INTO NUTRIENTS (ID,NAME,UNIT,CAT) \
               VALUES (32, 'Caffeine', 'mg', 'Other')");
 
-        basicStop()
+        self.commit()
+
+    def makeTestUsers(self):
+        self.conn.execute("INSERT INTO USERS (EMAIL, PASSWORD, AGE, SEX, HEIGHT, WEIGHT, ACTIVITY, NAME) VALUES ('a', 'aaaa', 1, 'F', 1, 1, 1, 'a')")
+        self.conn.execute("INSERT INTO USERS (EMAIL, PASSWORD, AGE, SEX, HEIGHT, WEIGHT, ACTIVITY, NAME) VALUES ('b', 'bbbb', 2, 'M', 2, 2, 2, 'b')")
+        self.commit()
 
     def setUserPassword(em, pwd):
-        conn = sqlite3.connect('pyfitnesspal.db')
-        conn.execute("INSERT INTO USERS (ID,EMAIL,PASSWORD, AGE, HEIGHT, WEIGHT, ACTIVITY, NAME) \
+        self.conn = sqlite3.connect('pyfitnesspal.db')
+        self.conn.execute("INSERT INTO USERS (ID,EMAIL,PASSWORD, AGE, HEIGHT, WEIGHT, ACTIVITY, NAME) \
               VALUES (1, ?, ?, 0, 0, 0, 0, 's')", (em, pwd));
-        conn.commit()
-        conn.close()
+        self.conn.commit()
+        self.conn.close()
 
     def getUserPassword():
-        conn = sqlite3.connect('pyfitnesspal.db')
-        cursor = conn.execute("SELECT PASSWORD FROM USERS")
+        self.conn = sqlite3.connect('pyfitnesspal.db')
+        cursor = self.conn.execute("SELECT PASSWORD FROM USERS")
         for row in cursor:
             return row[0]
-        conn.commit()
-        conn.close()
+        self.conn.commit()
+        self.conn.close()
 
 
 
 if __name__ == "__main__":
-    main()
+    db = Database()
+    db.makeNew()
+    db.makeTestUsers()
--- user.py
+++ user.py
@@ -1,36 +1,98 @@
 import database
+from random import randint
 
 class User:
     def __init__(self):
         self.db = database.Database()
-        email = input("enter your email: ")
-        pwd = input("enter your password: ")
-        age = input("enter your age: ")
-        sex = input("enter your sex: ")
-        h = input("enter your height: ")
-        w = input("enter your weight: ")
-        act = input("enter your activity level: ")
-        name = input("enter your name: ")
-        self.db.conn.execute("INSERT INTO USERS (EMAIL, PASSWORD, AGE, SEX, HEIGHT, WEIGHT, ACTIVITY, NAME) \
-              VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (email, pwd, age, sex, h, w, act, name));
-
-    """def setId(id):
-
-    def setEmail(email):
-
-    def setPassword(pwd):
-
-    def setAge(age):
-
-    def setSex(sex):
-
-    def setHeight(h):
-
-    def setWeight(w):
-
-    def setActivity(act):
-
-    def setName(name):"""
+        self.db.makeNew()
+        self.db.makeTestUsers()
 
 
-user = User()
+        self.setIden()
+
+
+        #email = input("enter your email: ")
+        #pwd = input("enter your password: ")
+        #age = input("enter your age: ")
+        #sex = input("enter your sex: ")
+        #h = input("enter your height: ")
+        #w = input("enter your weight: ")
+        #act = input("enter your activity level: ")
+        #name = input("enter your name: ")
+        #self.db.conn.execute("INSERT INTO USERS (EMAIL, PASSWORD, AGE, SEX, HEIGHT, WEIGHT, ACTIVITY, NAME) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (email, pwd, age, sex, h, w, act, name))
+
+        #self.setEmail(email)
+        #setPassword(pwd)
+        #setAge(age)
+        #setSex(sex)
+        #setHeight(h)
+        #setWeight(w)
+        #setActivity(act)
+        #setName(name)
+        #p = "aaaa"
+        # Set ID property to what's in database
+        #self.setId(self.getId(p), p)
+
+        self.db.close()
+
+
+    # Setters
+    def setIden(self):
+        i = randint(2, 999999999)
+        cursor = self.conn.execute("SELECT ID FROM USERS")
+        do:
+            self.__iden = i
+            self.db.conn.execute("INSERT INTO USERS (ID) VALUES (?)", (self.__iden,))
+        while (row[0] == i)
+
+
+    #def setEmail(self, email):
+        #self.db.conn.execute("UPDATE USERS set EMAIL = ? WHERE ID = ?", (email, self.id(self.password)))
+        #self.__email = email
+
+    #def setPassword(self, pwd):
+
+
+    #def setAge(self, age):
+
+    #def setSex(self, sex):
+
+    #def setHeight(self, h):
+
+    #def setWeight(self, w):
+
+    #def setActivity(self, act):
+
+    #def setName(self, name):
+
+
+    # Getters
+    def getIden(self):
+        return self.__iden
+
+    #def getEmail(self):
+        #return self.__email
+
+    #def getPassword(self):
+
+    #def getAge(self):
+
+    #def getSex(self):
+
+    #def getHeight(self):
+
+    #def getWeight(self):
+
+    #def getActivity(self):
+
+    #def getName(self):
+
+
+
+    # Properties
+    iden = property(fget = getIden)
+    #email = property(fset = setEmail, fget = setEmail)
+
+
+if __name__ == "__main__":
+    user = User()
